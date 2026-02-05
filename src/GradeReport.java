23243a13a2a625a7bc7a3f68655683f5c4ed62b1
Jeffery Anyimah Kwsai <jefferyanyimah15@gmail.com>
2026-02-05 23:43:46 +0000

    Demonstrating the ability of method overloading and procedural Abstraction

---
+++ src/GradeReport.java
@@ -0,0 +1,10 @@
+public static void executeGradeReport(double score){
+    if (!validateScore(score)){
+        System.out.println("Invalid Score");
+        return;
+    }
+
+    char grade = calculateLetterGrade(score);
+
+    displayPerformanceMessage(grade);
+}
